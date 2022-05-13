0a0de02d85ae24f5208d576b79653bbab2de266b
Weber <71677948+okok88515@users.noreply.github.com>
2022-05-13 13:25:22 +0800

    Add files via upload

--- demo/src/main/java/com/example/demo/kafka/JsonKafkaProducer.java
+++ demo/src/main/java/com/example/demo/kafka/JsonKafkaProducer.java
@@ -25,7 +25,7 @@ public class JsonKafkaProducer {
 
         Message<User> message = MessageBuilder
                 .withPayload(data)
-                .setHeader(KafkaHeaders.TOPIC, "javaguides")
+                .setHeader(KafkaHeaders.TOPIC, "javaguides_json")
                 .build();
 
         kafkaTemplate.send(message);
